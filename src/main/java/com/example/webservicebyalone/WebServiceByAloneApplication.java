9d38f07b8ec300c404f4c7ff8271d466059fa159
dev-hjkim <dev--test@naver.com>
2022-06-16 23:39:22 +0900

    first commit

---
+++ src/main/java/com/example/webservicebyalone/WebServiceByAloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicebyalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceByAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceByAloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webservicebyalone/WebServiceByAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicebyalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceByAloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
